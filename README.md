63347d36de6b99ebf8cad8387109270223e0c678
Marcelo Pereira <marcelo.fatecpoa@gmail.com>
2024-02-09 16:34:36 -0300

    docs: edit README file

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Notes
 
-Projeto desenvolvido no evento *NLW Expert* da Rocketseat que aconteceu de 5 a 8 de fevereiro de 2024.
+Projeto desenvolvido no evento _NLW Expert_ - Trielha de React da Rocketseat que aconteceu de 5 a 8 de fevereiro de 2024.
+
+![Screen desktop website](screenshot-desktop-01.png)
+
+## Sobre o projeto
+
+Aplicação front-end em ReactJS que permite o usuário salvar notas/lmbretes. As notas podem ser criadas tanto escritas como gravadas por voz.
+
+## Tecnologias
+
+- ReactJs
+- TypeScript
+- TailwindCSS
+- ViteJs
+- API Web Storage
+- API - Web Speech
+
+## Demo
+
+Este projeto foi publicado na **_Vercel_**.
+
+[Ver demo](https://notes.marcelopereira.dev)
--- src/components/ui/search/search.tsx
+++ src/components/ui/search/search.tsx
@@ -1,3 +1,4 @@
+import { MagnifyingGlass } from "@phosphor-icons/react";
 import { ChangeEvent } from "react";
 
 interface SearchProps {
@@ -7,13 +8,16 @@ interface SearchProps {
 export function Search({ handleSearch }: SearchProps) {
   return (
     <form>
-      <input
-        type="text"
-        placeholder="Buscar nota..."
-        aria-placeholder="Buscar nota..."
-        className="w-full bg-transparent text-xl lg:text-xl font-semibold tracking-tight placeholder:text-slate-500 py-2 outline-none border-b border-b-slate-700 focus-visible:border-b-lime-300 transition"
-        onChange={handleSearch}
-      />
+      <div className="flex items-center gap-2 border-b border-b-slate-700 focus-within:border-b-lime-300 transition">
+        <MagnifyingGlass weight="bold" className="text-slate-500 size-6" />
+        <input
+          type="text"
+          placeholder="Buscar nota..."
+          aria-placeholder="Buscar nota..."
+          className="w-full bg-transparent text-xl lg:text-xl font-semibold tracking-tight placeholder:text-slate-500 py-2 outline-none"
+          onChange={handleSearch}
+        />
+      </div>
     </form>
   );
 }
